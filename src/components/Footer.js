492e6c23617280bfd64740a0f3f3c8b0d0976d27
mitch-davies <mdavies259@gmail.com>
2023-01-30 09:46:18 +0200

    fixed warnings and prepped for production build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
   return (
     <div className='footer-container'>
         <div class='footer-link-items'>
-            <a className='social-icon-link' href="https://instagram.com/mitchdrxms" target='blank'><i class="fa fa-instagram" aria-hidden="true"></i></a>
-            <a href="https://www.linkedin.com/in/mitch-dav/" target='blank' alt="Instagram"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+            <a className='social-icon-link' href="https://instagram.com/mitchdrxms" target='blank'><i class="fa fa-instagram" aria-hidden="true"></i> </a>
+            <a href="https://www.linkedin.com/in/mitch-dav/" target='blank' alt="Instagram"><i class="fa fa-linkedin" aria-hidden="true"></i> </a>
         </div>
         <div className='website-rights'>
         <small>Mitchell Davies © 2023</small>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,24 +5,11 @@ import './Navbar.css';
 
 function Navbar() {
 const [click, setClick] = useState(false);
-// const [button, setButton] = useState(true)
+
 
 const handleClick = () => setClick(!click);
 const closeMobileMenu = () => setClick(false);
 
-// const showButton = () => {
-//     if(window.innerWidth <= 960) {
-//         setButton(false)
-//     } else {
-//         setButton(true);
-//     }
-// };
-
-// useEffect(() => {
-//     showButton()
-// }, [])
-
-// window.addEventListener('resize', showButton);
 
   return (
     <>
@@ -46,7 +33,6 @@ const closeMobileMenu = () => setClick(false);
                         <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>Contact</Link>
                     </li>
                 </ul>
-                {/* {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>} */}
             </div>
         </nav>
     </>
